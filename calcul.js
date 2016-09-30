5f090abd95bbd9b27c65fe6f981491b2877ff891
bansalhimanshu75 <himanshu.bansal@kelltontech.com>
2016-09-30 12:58:59 +0530

    Add files via upload

---
+++ calcul.js
@@ -0,0 +1,22 @@
+module.exports = {
+  sum:function(a,b){
+  var c = a + b;
+  return c;
+  },
+  minus:function(a,b){
+  var c = a - b;
+  return c;
+  },
+  multiply:function(a,b){
+  var c = a * b;
+  return c;
+  },
+  divide:function(a,b){
+  var c = a/b;
+  return c;
+  },
+  modulus:function(a,b){
+  var c = a % b;
+  return c;
+  },
+}; 
